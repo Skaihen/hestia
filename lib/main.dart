63d02cfe429158e76c2e1bad19fe439026db5734
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-08-30 14:35:15 +0200

    l10n

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,20 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Opacity(
+            opacity: 0.5,
+            child: Column(
+              children: [
+                SizedBox(height: 64),
+                Icon(Icons.emoji_food_beverage_outlined, size: 48),
+                SizedBox(height: 16),
+                Text(S.of(context).noTasksYet),
+              ],
+            ),
+          ),
         ),
       ),
     );
