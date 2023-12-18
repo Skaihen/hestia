8df6145cee9c19d0767d8fc6f1b55a6ed6c05402
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-12-18 17:18:52 +0100

    init

---
+++ src/hestia/app.py
@@ -0,0 +1,20 @@
+"""
+Multiplatform recipe book
+"""
+import toga
+from toga.style import Pack
+from toga.style.pack import COLUMN, ROW
+
+
+class Hestia(toga.App):
+
+    def startup(self):
+        main_box = toga.Box()
+
+        self.main_window = toga.MainWindow(title=self.formal_name)
+        self.main_window.content = main_box
+        self.main_window.show()
+
+
+def main():
+    return Hestia()
