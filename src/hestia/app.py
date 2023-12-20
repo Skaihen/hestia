72ab38598129a28a849020cb4e90be0e1297b95c
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-12-20 16:17:01 +0100

    kivy

--- src/hestia/app.py
+++ src/hestia/app.py
@@ -1,44 +1,18 @@
-"""
-Multiplatform recipe book
-"""
-import toga
-from toga.style.pack import Pack
-from travertino.constants import COLUMN, ROW
+from kivymd.app import MDApp
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.button import MDRectangleFlatButton
 
 
-def greeting(name):
-    if name:
-        return f"Hello, {name}"
-    else:
-        return "Hello, stranger"
+class MainApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Orange"
 
-
-class Hestia(toga.App):
-    def startup(self):
-        main_box = toga.Box(style=Pack(direction=COLUMN))
-
-        name_label = toga.Label("Prueba", style=Pack(padding=(0, 5)))
-
-        self.name_input = toga.TextInput(style=Pack(flex=1))
-
-        name_box = toga.Box(style=Pack(direction=ROW, padding=5))
-        name_box.add(name_label)
-        name_box.add(self.name_input)
-
-        button = toga.Button(
-            "Say Hello!", on_press=self.say_hello, style=Pack(padding=5)
+        return MDScreen(
+            MDRectangleFlatButton(
+                text="Hello, World", pos_hint={"center_x": 0.5, "center_y": 0.5}
+            )
         )
 
-        main_box.add(name_box)
-        main_box.add(button)
 
-        self.main_window = toga.MainWindow(title=self.formal_name)
-        self.main_window.content = main_box
-        self.main_window.show()
-
-    def say_hello(self, widget):
-        self.main_window.info_dialog(greeting(self.name_input.value), "Hi there!")
-
-
-def main():
-    return Hestia()
+MainApp().run()
